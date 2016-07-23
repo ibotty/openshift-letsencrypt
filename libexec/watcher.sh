877ba577477d3b68fc5cff6d6719d5dcf5bca579
Tobias Florek <tob@butter.sh>
2016-07-23 10:17:08 +0200

    wip

--- libexec/watcher.sh
+++ libexec/watcher.sh
@@ -3,13 +3,11 @@
 . "$LETSENCRYPT_SHAREDIR/common.sh"
 set -o pipefail
 
-OC_ROUTES_OPTIONS=("-o" "go-template-file=$LETSENCRYPT_SHAREDIR/process-new-route.yaml")
-
 watch_routes() {
     local domainname namespace name
-    oc_get_routes "${OC_ROUTES_OPTIONS[@]}" \
-        --template='{{range .items}}{{.spec.host}}:{{.metadata.namespace}}:{{.metadata.name}}
-{{end}}' \
+    oc_get_routes --watch-only \
+        --template='{{.spec.host}}:{{.metadata.namespace}}:{{.metadata.name}}
+' \
         | while IFS=: read -r domainname namespace name; do
             get_certificate "$domainname"
             add_certificate_to_route "$domainname" "$namespace" "$name"
